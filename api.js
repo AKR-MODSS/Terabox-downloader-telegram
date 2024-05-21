a1083f0708f51f47ad3d425623abeca3f63a2b3d
AKR-MODSS <goldp3rl@gmail.com>
2024-05-21 22:28:20 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://www.terabox.com/share/list?app_id=250528&shorturl`
+            `https://www.terabox.com/share/list?app_id=250528&shorturl={key}&root=1`
         );
         return response.data;
     } catch (error) {
